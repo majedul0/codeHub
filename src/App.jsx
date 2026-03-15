444e2877872e75a5fa0b6852ca23681a42122bcc
majedul0 <islam22205101253@diu.edu.bd>
2026-03-15 14:13:40 +0600

    refactor App component to use BrowserRouter and ProjectRoutes for improved routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter } from "react-router-dom";
+import ProjectRoutes from "./Routes";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      Home Page
-    </>
-  )
+    <BrowserRouter>
+      <ProjectRoutes />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
